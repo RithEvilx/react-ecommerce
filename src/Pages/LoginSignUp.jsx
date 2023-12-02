919905ac23a6b3ae3d31522f163070395c9d4ef5
RithEvil <sovannarith22317@gmail.com>
2023-12-02 22:58:15 +0700

    add breadcrumb and design display page

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./CSS/LoginSignUp.css";
 
 const LoginSignUp = () => {
   return (
-    <div>
-      
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login in here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree the terms of use & privacy policy</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignUp
+export default LoginSignUp;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext.jsx";
+import { useParams } from "react-router-dom";
+import Breadcrumbs from "../Components/Breadcrumbs/Breadcrumbs.jsx";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay.jsx";
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
   return (
     <div>
-      
+      <Breadcrumbs product={product} />
+      <ProductDisplay product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
