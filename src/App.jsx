e49c6541017a082e6ffb8cace7665477598151f6
RithEvil <sovannarith22317@gmail.com>
2023-12-02 16:35:15 +0700

    add more pages

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop.jsx";
+import Product from "./Pages/Product.jsx";
+import ShopCategory from "./Pages/ShopCategory.jsx";
+import Cart from "./Pages/Cart.jsx";
+import LoginSignUp from "./Pages/LoginSignUp.jsx";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
